1ccf43f9cd14d03e3f18b70feceded416adfac61
alex <alexdev12@yandex.ru>
2019-06-17 19:22:15 +0300

    hairing components

---
+++ app/src/components/app/app.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import AppHeader from '../app-header/app-header'
+import SearchPanel from '../search-panel/search-panel'
+import TodoList from '../todo-list/todo-list'
+import ItemStatusFilter from '../item-status-filter/item-status-filter'
+
+
+const App = () => {
+    const todoData = [
+        {label: 'Drink Coffee', important: false, id: 1},
+        {label: 'Make app', important: true, id: 2},
+        {label: 'Have a lunch', important: false, id: 3},
+    ]
+    return (
+        <div className="todo-app">
+            <AppHeader />
+            <div className="search-panel d-flex">
+                <SearchPanel />
+                <ItemStatusFilter/>
+            </div>
+            <TodoList todos={todoData}/>
+        </div>
+    )
+}
+
+export default App
---
+++ app/src/components/todo-list-item/todo-list-item.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './todo-list-item.css'
+
+const TodoListItem = ({ label, important = false }) => {
+    const style = {
+        color: important ? 'tomato' : 'black'
+    }
+    return (
+        <span className="todo-list-item">
+            <span className="todo-list-item-label" style={style} >{label}</span>
+            <button type="button" className="btn btn-outline-sucess btn-small">
+                <i className="fa fa-exclamation"></i>
+            </button>
+            <button type="button" className="btn btn-outline-danger btn-small">
+                <i className="fa fa-trash-o"></i>
+            </button>
+        </span>
+    )
+}
+
+export default TodoListItem
